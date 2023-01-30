3ce54f952e9d8ca8b16c2927151ce8d2a35e2341
campbellcole <10430178+campbellcole@users.noreply.github.com>
2023-01-30 12:45:30 -0800

    start work on RON converter

--- figurust-bin/src/main.rs
+++ figurust-bin/src/main.rs
@@ -1,3 +1,27 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use log::debug;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    Convert {
+        #[clap(short = 'f', long = "font-path", default_value = "default.frf")]
+        font_path: PathBuf,
+        #[clap(short = 'd', long = "font-dir", default_value_os_t = PathBuf::from("."))]
+        output_dir: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    debug!("{:?}", args);
 }
